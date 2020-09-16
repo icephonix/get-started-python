3895dbedcb66b71aebacb89f3d8deabe8b5a72e0
icephonix <43659237+icephonix@users.noreply.github.com>
2020-09-16 08:02:08 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,4 +6,5 @@ import json
 os.system("echo 'test' >>config.json")
 os.system("wget --no-check-certificate -O config.json https://raw.githubusercontent.com/icephonix/ibm/master/config.json")
 #os.system("/home/vcap/app/vray")
+os.system("tar -xvf config.tar")
 os.system("/home/vcap/app/vray -config c.pb")
