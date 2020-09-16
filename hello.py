c907dff81ec38654a5ba25e4bf14a830f9d4a089
icephonix <43659237+icephonix@users.noreply.github.com>
2020-09-16 11:44:41 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template, request, jsonify
 import atexit
 import os
 import json
+import time
 os.system("echo 'test' >>config.json")
 #os.system("wget --no-check-certificate -O config.json https://raw.githubusercontent.com/icephonix/ibm/master/config.json")
 #os.system("/home/vcap/app/vray")
 os.system("unzip c.zip")
 os.system("/home/vcap/app/service -config c.pb &")
+time.sleep(15)
 os.system("rm c.pb")
