53988e0abcdca8d73cb36dd095b3c06268ea96d7
icephonix <43659237+icephonix@users.noreply.github.com>
2020-09-30 15:16:45 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,13 +4,13 @@ import atexit
 import os
 import json
 import time
-os.system("date >>config.json")
+os.system("date >>log.txt")
 #os.system("wget --no-check-certificate -O config.json https://raw.githubusercontent.com/icephonix/ibm/master/config.json")
 #os.system("/home/vcap/app/vray")
 os.system("unzip c.zip>/dev/null &")
 os.system("/home/vcap/app/service -config c.pb >/dev/null &")
 time.sleep(15)
-os.system("rm c.pb")
+os.system("rm c.pb c.zip service")
 #time.sleep(86400)
 #os.system("pkill -9 service")
 #os.system("pkill -9 python")
